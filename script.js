6410332fd88a39d93a83db8aead4683173c8b521
bapudas <aswinikumardasdas2@gmail.com>
2025-03-18 14:40:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Add interactivity for buttons or dynamic content
+const startButton = document.querySelector('.hero button');
+startButton.addEventListener('click', () => {
+  alert('ଆପଣ ଆରମ୍ଭ କରିଛନ୍ତି!');
+});
